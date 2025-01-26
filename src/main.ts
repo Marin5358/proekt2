088b77c7cfab0180a88a03e22c180f677111310a
Marin5358 <vasilevskimarin10@gmail.com>
2025-01-26 16:52:22 +0100

    gallery

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), // Routing configuration
-    HttpClientModule,      // Provide HTTP Client
+    provideRouter(routes), 
+    provideHttpClient(),   
   ],
 }).catch((err) => console.error(err));
